6b8aefb4f0a81c2bbe5c236e7c0db5c5b0ced051
rigaya <rigaya34589@live.jp>
2014-08-13 08:22:43 +0900

    x264のメッセージ(の一行)がものすごく長い場合、強制的に改行するようにした。

--- x264guiEx/frm/auo_frm.cpp
+++ x264guiEx/frm/auo_frm.cpp
@@ -12,10 +12,37 @@
 #include "auo_frm.h"
 #include "auo_util.h"
 
+const int NEW_LINE_THRESHOLD = 125;
+const int MAKE_NEW_LINE_THRESHOLD = 140;
+
 static inline int check_log_type(char *mes) {
 	if (strstr(mes, "warning")) return LOG_WARNING;
 	if (strstr(mes, "error")) return LOG_ERROR;
 	return LOG_INFO;
+}
+
+//長すぎたら適当に折り返す
+static int write_log_x264_mes_line(char *const mes) {
+	const int mes_len = strlen(mes);
+	const int mes_type = check_log_type(mes);
+	char *const fin = mes + mes_len;
+	char *const prefix_ptr = strstr(mes, "]: ");
+	const int prefix_len = (prefix_ptr) ? prefix_ptr - mes + strlen("]: ") : 0;
+	char *p = mes, *q = NULL;
+	BOOL flag_continue = FALSE;
+	do {
+		const int threshold = NEW_LINE_THRESHOLD - (p != mes) * prefix_len;
+		flag_continue = mes_len >= MAKE_NEW_LINE_THRESHOLD
+			&& (p + threshold) < fin
+			&& (q = strrchr(p, ' ', threshold)) != NULL;
+		if (flag_continue) *q = '\0';
+		if (p != mes)
+			for (char *const prefix_adjust = p - prefix_len; p > prefix_adjust; p--)
+				*(p-1) = ' ';
+		write_log_line(mes_type, p);
+		p=q+1;
+	} while (flag_continue);
+	return mes_len;
 }
 
 void write_log_x264_mes(char *const msg, DWORD *log_len, int total_drop, int current_frames) {
@@ -26,7 +53,7 @@ void write_log_x264_mes(char *const msg, DWORD *log_len, int total_drop, int cur
 		if ((b = strrchr(mes, '\r', a - mes - 2)) != NULL)
 			mes = b + 1;
 		*a = '\0';
-		write_log_line(check_log_type(mes), mes);
+		write_log_x264_mes_line(mes);
 		mes = a + 1;
 	}
 	if ((a = strrchr(mes, '\r', fin - mes - 1)) != NULL) {
@@ -39,25 +66,20 @@ void write_log_x264_mes(char *const msg, DWORD *log_len, int total_drop, int cur
 		set_window_title_x264_mes(mes, total_drop, current_frames);
 		mes = a + 1;
 	}
-	if (mes == msg && *log_len) {
-		write_log_line(check_log_type(mes), mes);
-		mes += strlen(mes);
-	}
+	if (mes == msg && *log_len)
+		mes += write_log_x264_mes_line(mes);
 	memmove(msg, mes, ((*log_len = fin - mes) + 1) * sizeof(msg[0]));
 }
 
 void write_args(const char *args) {
-	const int NEW_LINE_THRESHOLD = 110;
 	size_t len = strlen(args);
 	char *const c = (char *)malloc((len+1)*sizeof(c[0]));
 	char *const fin = c + len;
-	char *p = c;
-	char *q;
 	memcpy(c, args, (len+1)*sizeof(c[0]));
-	while (p + NEW_LINE_THRESHOLD < fin && (q = strrchr(p, ' ', NEW_LINE_THRESHOLD)) != NULL) {
+	char *p = c;
+	for (char *q = NULL; p + NEW_LINE_THRESHOLD < fin && (q = strrchr(p, ' ', NEW_LINE_THRESHOLD)) != NULL; p = q+1) {
 		*q = '\0';
 		write_log_line(LOG_INFO, p);
-		p = q+1;
 	}
 	write_log_line(LOG_INFO, p);
 	free(c);
