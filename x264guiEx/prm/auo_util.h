c230fa5927896d1d26a190c1e53fc20cb801efb8
rigaya <rigaya34589@live.jp>
2014-08-14 19:36:54 +0900

    VC++2012を必須に、またVC++2012できちんとコンパイルできるように

--- x264guiEx/prm/auo_util.h
+++ x264guiEx/prm/auo_util.h
@@ -11,7 +11,9 @@
 #define _AUO_UTIL_H_
 
 #include <Windows.h>
+#if (_MSC_VER >= 1800)
 #include <VersionHelpers.h>
+#endif
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -296,7 +298,6 @@ static BOOL check_sse4_1() {
 	__cpuid(CPUInfo, 1);
 	return (CPUInfo[2] & 0x00080000) != 0;
 }
-#if (_MSC_VER >= 1600)
 static BOOL check_avx() {
 	int CPUInfo[4];
 	__cpuid(CPUInfo, 1);
@@ -307,8 +308,6 @@ static BOOL check_avx() {
 	}
 	return FALSE;
 }
-#endif
-#if (_MSC_VER >= 1700)
 static BOOL check_avx2() {
 	int CPUInfo[4];
 	__cpuid(CPUInfo, 1);
@@ -322,7 +321,6 @@ static BOOL check_avx2() {
 	}
 	return FALSE;
 }
-#endif
 
 static DWORD get_availableSIMD() {
 	int CPUInfo[4];
@@ -338,7 +336,6 @@ static DWORD get_availableSIMD() {
 		simd |= AUO_SIMD_SSE41;
 	if  (CPUInfo[2] & 0x00100000)
 		simd |= AUO_SIMD_SSE42;
-#if (_MSC_VER >= 1600)
 	UINT64 XGETBV = 0;
 	if ((CPUInfo[2] & 0x18000000) == 0x18000000) {
 		XGETBV = _xgetbv(0);
@@ -348,12 +345,18 @@ static DWORD get_availableSIMD() {
 	__cpuid(CPUInfo, 7);
 	if ((simd & AUO_SIMD_AVX) && (CPUInfo[1] & 0x00000020))
 		simd |= AUO_SIMD_AVX2;
-#endif
 	return simd;
 }
 
 static BOOL check_OS_Win7orLater() {
+#if (_MSC_VER >= 1800)
 	return IsWindowsVersionOrGreater(6, 1, 0);
+#else
+	OSVERSIONINFO osvi = { 0 };
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	GetVersionEx(&osvi);
+	return ((osvi.dwPlatformId == VER_PLATFORM_WIN32_NT) && ((osvi.dwMajorVersion == 6 && osvi.dwMinorVersion >= 1) || osvi.dwMajorVersion > 6));
+#endif
 }
 
 static inline const char *GetFullPath(const char *path, char *buffer, size_t nSize) {
