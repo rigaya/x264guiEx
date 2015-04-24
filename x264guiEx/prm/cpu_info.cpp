e08ba7da136b8f937f8b52334a2f4bb11dccde03
rigaya <rigaya34589@live.jp>
2015-04-24 07:46:18 +0530

    環境によってはrdtscpを使うと0xc0000096例外で落ちることがあるのを回避。

--- x264guiEx/prm/cpu_info.cpp
+++ x264guiEx/prm/cpu_info.cpp
@@ -242,6 +242,18 @@ static unsigned int __stdcall getCPUClockMaxSubFunc(void *arg) {
 	return 0;
 }
 
+//rdtscpを使うと0xc0000096例外 (一般ソフトウェア例外)を発する場合があるらしい
+//そこでそれを検出する
+bool check_rdtscp_available() {
+	__try {
+		UINT dummy;
+		__rdtscp(&dummy);
+	} __except (EXCEPTION_EXECUTE_HANDLER) {
+		return false;
+	}
+	return true;
+}
+
 //__rdtscが定格クロックに基づいた値を返すのを利用して、実際の動作周波数を得る
 //やや時間がかかるので注意
 double getCPUMaxTurboClock(unsigned int num_thread) {
@@ -263,6 +275,10 @@ double getCPUMaxTurboClock(unsigned int num_thread) {
 	//rdtscp命令のチェック (Fn:8000_0001:EDX27)
 	__cpuid(CPUInfo, 0x80000001);
 	if (0 == (CPUInfo[3] & (1<<27))) {
+		return defaultClock;
+	}
+	//例外が発生するなら処理を中断する
+	if (!check_rdtscp_available()) {
 		return defaultClock;
 	}
 
