d6702a3e99523def339dbdf4b27891e496268407
rigaya <rigaya34589@live.jp>
2022-03-16 18:57:02 +0900

    念のためのチェックを追加。

--- x264guiEx/frm/auo_error.cpp
+++ x264guiEx/frm/auo_error.cpp
@@ -171,11 +171,13 @@ void error_invalid_ini_file() {
 }
 
 void error_unsupported_audio_format_by_muxer(const int video_out_type, const char *selected_aud, const char *default_aud) {
-    write_log_auo_line_fmt(LOG_ERROR, "音声エンコーダ %s は、%s 形式での出力に対応していません。", selected_aud, OUTPUT_FILE_EXT[video_out_type] + 1);
-    if (default_aud) {
-        write_log_auo_line_fmt(LOG_ERROR, "%s 等の他の音声エンコーダを選択して出力してください。", default_aud);
-    } else {
-        write_log_auo_line(LOG_ERROR, "他の音声エンコーダを選択して出力してください。");
+    if (video_out_type < _countof(OUTPUT_FILE_EXT)) {
+        write_log_auo_line_fmt(LOG_ERROR, "音声エンコーダ %s は、%s 形式での出力に対応していません。", selected_aud, OUTPUT_FILE_EXT[video_out_type] + 1);
+        if (default_aud) {
+            write_log_auo_line_fmt(LOG_ERROR, "%s 等の他の音声エンコーダを選択して出力してください。", default_aud);
+        } else {
+            write_log_auo_line(LOG_ERROR, "他の音声エンコーダを選択して出力してください。");
+        }
     }
 }
 
