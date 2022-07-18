3b650d77c586642175c15e09f17b8bc1329bec09
rigaya <rigaya34589@live.jp>
2022-07-18 11:44:55 +0900

    convert.hの共通化。

--- x264guiEx/encode/convert.h
+++ x264guiEx/encode/convert.h
@@ -28,6 +28,10 @@ typedef struct {
     int   count;       //planarの枚数。packedなら1
     BYTE *data[3];     //planarの先頭へのポインタ
     int   size[3];     //planarのサイズ
+#if ENCODER_X265
+    int   w[3], h[3], pitch[3];
+    int byte_per_pixel;
+#endif
     int   total_size;  //全planarのサイズの総和
 } CONVERT_CF_DATA;
 
